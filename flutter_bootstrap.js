6d9f37e0ab88803940795c51470ad9099246115e
백승일 <sibaek@smilegate.com>
2024-09-20 15:35:24 +0900

    new design

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2713811498"
+    serviceWorkerVersion: "1750216461"
   }
 });
